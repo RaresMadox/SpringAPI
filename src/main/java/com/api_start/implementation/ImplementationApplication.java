2e797ce4fe0a2638cffcc097f98340d36e40d548
Petre Raes <rares.petre@guillemot.ro>
2024-11-28 11:29:42 +0200

    Project Commit

---
+++ src/main/java/com/api_start/implementation/ImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.api_start.implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_start/implementation/ImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_start.implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
